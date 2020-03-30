2731f089e82a878808896054f253ebc89226b2bc
renatoalves0 <renatoalves97.123@gmail.com>
2020-03-30 14:51:19 -0300

    Dados pessoais + endereço

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -19,10 +19,12 @@
             body-classes="px-lg-5 py-lg-5"
             class="border-0"
           >
+            <h4 class="mb-4 text-success font-weight-bold">Dados Pessoais</h4>
             <template>
               <form role="form">
                 <base-input class="mb-3" placeholder="Nome" v-model="pessoa.nome"></base-input>
                 <base-input class="mb-3" placeholder="Email" v-model="pessoa.email"></base-input>
+                <base-input class="mb-3" placeholder="Telefone" v-model="pessoa.telefone"></base-input>
                 <base-radio name="fisica" class="mb-3" v-model="pessoa.tipo">Pessoa Física</base-radio>
                 <base-radio name="juridica" class="mb-3" v-model="pessoa.tipo">Pessoa Jurídica</base-radio>
                 <base-input
@@ -52,8 +54,38 @@
                   type="password"
                   placeholder="Confirmar Senha"
                   addon-left-icon="ni ni-lock-circle-open"
-                  v-model="pessoa.confirmacao_senha"
+                  v-model="confirmacao_senha"
                 ></base-input>
+                <div class="text-center">
+                  <base-button type="success" class="my-4">Cadastrar-me</base-button>
+                </div>
+              </form>
+            </template>
+          </card>
+        </div>
+
+        <div class="col-lg-5">
+          <card
+            shadow
+            header-classes="bg-white pb-5"
+            body-classes="px-lg-5 py-lg-5"
+            class="border-0"
+          >
+            <h4 class="mb-4 text-success font-weight-bold">Endereço</h4>
+            <template>
+              <form role="form">
+                <base-input class="mb-3" placeholder="País" v-model="endereco.pais"></base-input>
+                <base-input class="mb-3" placeholder="Estado" v-model="endereco.estado"></base-input>
+                <base-input class="mb-3" placeholder="Cidade" v-model="endereco.cidade"></base-input>
+                <base-input
+                  class="mb-3"
+                  placeholder="CEP"
+                  v-mask="'#####-###'"
+                  v-model="endereco.cep"
+                ></base-input>
+                <base-input class="mb-3" placeholder="Bairro" v-model="endereco.bairro"></base-input>
+                <base-input class="mb-3" placeholder="Logradouro" v-model="endereco.logradouro"></base-input>
+                <base-input class="mb-3" placeholder="Número" v-model="endereco.numero"></base-input>
                 <div class="text-center">
                   <base-button type="success" class="my-4">Cadastrar-me</base-button>
                 </div>
@@ -70,17 +102,27 @@ export default {
   data() {
     return {
       pessoa: {
-        colaborador: undefined,
-        investidor: undefined,
-        cliente: undefined,
-        tipo: "fisica",
+        colaborador: Boolean,
+        investidor: Boolean,
+        cliente: Boolean,
+        tipo: "fisica", //Pode assumir os valores: "fisica" ou "juridica"
         cpf: "",
         cnpj: "",
         nome: "",
         email: "",
-        senha: "",
-        confirmacao_senha: ""
-      }
+        telefone: "",
+        senha: ""
+      },
+      endereco: {
+        pais: "",
+        estado: "",
+        cidade: "",
+        cep: "",
+        bairro: "",
+        logradouro: "",
+        numero: ""
+      },
+      confirmacao_senha: ""
     };
   }
 };

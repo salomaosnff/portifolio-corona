4b5525d05c728c53ae415e5c03154ae47a0fc34b
edgarsay <seyedgarend@gmail.com>
2020-05-06 20:44:43 -0300

    Investidor

--- src/views/Colaborador.vue
+++ src/views/Colaborador.vue
@@ -15,27 +15,27 @@
         <div class="col-lg-4">
           <card class="border-0" hover shadow body-classes="py-5">
             <icon name="ni ni-badge" type="warning " rounded class="mb-4" />
-            <h4 class="text-warning">{{$t("Colaborador.text-warning")}}</h4>
-            <p class="description mt-3">{{$t("Colaborador.description")}}</p>
+            <h4 class="text-warning">{{$t("Colaborador||Investidor.text-warning")}}</h4>
+            <p class="description mt-3">{{$t("Colaborador||Investidor.description")}}</p>
             <div>
-              <badge type="warning" rounded>{{$t("Colaborador.badge_warning1")}}</badge>
-              <badge type="warning" rounded>{{$t("Colaborador.badge_warning2")}}</badge>
-              <badge type="warning" rounded>{{$t("Colaborador.badge_warning3")}}</badge>
+              <badge type="warning" rounded>{{$t("Colaborador||Investidor.badge_warning1")}}</badge>
+              <badge type="warning" rounded>{{$t("Colaborador||Investidor.badge_warning2")}}</badge>
+              <badge type="warning" rounded>{{$t("Colaborador||Investidor.badge_warning3")}}</badge>
             </div>
-            <base-button tag="a" href="#/register" type="warning" class="mt-4">{{$t("Colaborador.button_register")}}</base-button>
+            <base-button tag="a" href="#/register" type="warning" class="mt-4">{{$t("Colaborador||Investidor.button_register")}}</base-button>
           </card>
         </div>
         <div class="col-lg-4">
           <card class="border-0" hover shadow body-classes="py-5">
             <icon name="ni ni-fat-add" type="primary" rounded class="mb-4" />
-            <h4 class="text-primary">{{$t("Colaborador.text-primary")}}</h4>
-            <p class="description mt-3">{{$t("Colaborador.description")}}</p>
+            <h4 class="text-primary">{{$t("Colaborador||Investidor.text-primary")}}</h4>
+            <p class="description mt-3">{{$t("Colaborador||Investidor.description")}}</p>
             <div>
-              <badge type="warning" rounded>{{$t("Colaborador.badge_warning1")}}</badge>
-              <badge type="warning" rounded>{{$t("Colaborador.badge_warning2")}}</badge>
-              <badge type="warning" rounded>{{$t("Colaborador.badge_warning3")}}</badge>
+              <badge type="warning" rounded>{{$t("Colaborador||Investidor.badge_warning1")}}</badge>
+              <badge type="warning" rounded>{{$t("Colaborador||Investidor.badge_warning2")}}</badge>
+              <badge type="warning" rounded>{{$t("Colaborador||Investidor.badge_warning3")}}</badge>
             </div>
-            <base-button tag="a" href="#/solucoes_cadastro" type="primary" class="mt-4">{{$t("Colaborador.button_solucoes_cadastro")}}</base-button>
+            <base-button tag="a" href="#/solucoes_cadastro" type="primary" class="mt-4">{{$t("Colaborador||Investidor.button_solucoes_cadastro")}}</base-button>
           </card>
         </div>
       </div>

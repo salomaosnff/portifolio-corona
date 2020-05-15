160aba69165a566c8cb426c114d31f2cd7ca3146
renatoalves0 <renatoalves97.123@gmail.com>
2020-05-15 12:54:30 -0300

    Correção no link

--- src/services/http.js
+++ src/services/http.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const baseUrl = [
-    'http://localhost:3000/',
     'https://portifolio-corona-api.herokuapp.com/',
+    'http://localhost:3000/',
 ]
 
 export default class Http {
